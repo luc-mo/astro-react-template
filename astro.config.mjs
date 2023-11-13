dd4acf1637b650e83fe87218beb9e2c573cc42c5
SnowDrive <luc.moralesr@gmail.com>
2023-11-13 12:19:16 -0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import unocss from 'unocss/astro'
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    unocss({ injectReset: true }),
+    react(),
+  ]
+});
